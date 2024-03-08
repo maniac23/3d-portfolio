ddfe924067c2cae4ffcfa21839c9f07861094098
maniac23 <tukaym@gmail.com>
2024-03-08 21:06:35 +0300

    Island rotation + Bird and Plane animations

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,14 +1,26 @@
-import { useGLTF } from '@react-three/drei'
-import React from 'react';
+import { useGLTF } from '@react-three/drei';
+import {useRef} from 'react';
+import { useFrame } from '@react-three/fiber';
 import skyScene from '../assets/3d/sky.glb';
 
-const Sky = () => {
-  const sky = useGLTF(skyScene);
-  return (
-   <mesh>
-    <primitive object={sky.scene} />
-   </mesh>
-  )
-}
 
-export default Sky
+const Sky = ({ isRotating, ...props }) => {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  });
+
+
+
+  return (
+    <mesh ref={skyRef}>
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+};
+
+export default Sky;

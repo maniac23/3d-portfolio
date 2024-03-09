b58ef58cce01a5dc6ab2460b5721bcfa38762444
maniac23 <tukaym@gmail.com>
2024-03-09 17:24:13 +0300

    Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,49 @@
+import { Link } from 'react-router-dom';
+import { projects } from '../constants';
+import {arrow} from "../assets/icons";
+import CTA from '../components/CTA';
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        My <span className="blue-gradient_text font-semibold drop-shadow">Projects</span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          I've embarked numerous projects over the years, but these are the ones I hold closest to
+          my heart. Many of them are open-source, so if you come across something that piques your
+          interest, feel free to check it out and contribute you ideas for further enchancements.
+          Your collaboration is always welcome!
+        </p>
+      </div>
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project) => (
+          <div className='lg:w-[400px] w-full' key={project.name}>
+            <div className='block-container w-12 h-12'>
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={project.iconUrl}
+                  alt="Project icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+            <div className='mt-5 flex flex-col'>
+              <h4 className='text-2xl font-poppins font-semibold'>{project.name}</h4>
+              <p className='mt-2 text-slate-500'>{project.description}</p>
+              <div className='mt-5 flex items-center gap-2 font-poppins'>
+                <Link to={project.link} target='_blank' rel='noopener noreferrer' className='fort-semibold text-blue-600'>Live link</Link>
+                <img src={arrow} alt="arrow"  className='w-4 h-4 object-contain'/>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr className='border-slate-200' />
+      <CTA />
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;

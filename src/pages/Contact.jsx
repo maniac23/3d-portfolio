cff6703db55dd0a97cbe8ee231276f1f820f6177
maniac23 <tukaym@gmail.com>
2024-03-09 15:53:55 +0300

    Fox animation and alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,10 @@
-import { useRef, useState } from 'react';
-import emailjs from "@emailjs/browser";
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import { Suspense, useRef, useState } from 'react';
+import Alert from '../components/Alert';
+import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Fox from '../models/Fox';
 
 const Contact = () => {
   const formRef = useRef();
@@ -10,6 +15,9 @@ const Contact = () => {
   });
 
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+  const {alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({
@@ -18,39 +26,54 @@ const Contact = () => {
     });
   };
 
-  const handleFocus = () => {};
+  const handleFocus = () => {
+    setCurrentAnimation('walk');
+  };
 
-  const handleBlur = () => {};
+  const handleBlur = () => {
+    setCurrentAnimation('idle');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    emailjs.send(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: 'Mikhail',
-        from_email: form.email,
-        to_email: 'mikhail.tukai@icloud.com',
-        message: form.message,
-      },
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(() => {
-      setIsLoading(false);
-      setForm({name: "", email: "", message: ""});
-      // show success message;
-      // hide an alert
-    }).catch((e) => {
-      setIsLoading(false);
-      console.log(e);
-      // show error message
-    })
-
-  }
+    setCurrentAnimation('hit');
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Mikhail',
+          from_email: form.email,
+          to_email: 'mikhail.tukai@icloud.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        showAlert({show: true, text: 'Message sent', type: 'success'});
+        // show success message;
+        // hide an alert
+        setTimeout(() => {
+          hideAlert();
+          setCurrentAnimation('idle');
+          setForm({ name: '', email: '', message: '' });
+        }, 3000);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setCurrentAnimation('idle');
+        console.log(e);
+        // show error message
+        showAlert({show: true, text: 'Ooops! Something went wrong 🥺', type: 'danger'});
+      });
+  };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in touch</h1>
         <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit} ref={formRef}>
@@ -105,6 +128,20 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+              currentAnimation={currentAnimation}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
